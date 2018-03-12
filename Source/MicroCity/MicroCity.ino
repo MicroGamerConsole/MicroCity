cc78441268beb7655a342b110f726baf4307ad6c
Fabien Chouteau <fabien.chouteau@gmail.com>
2018-03-13 00:08:37 +0100

    Add game load/save support on the Micro:Gamer

--- Source/MicroCity/MicroCity.ino
+++ Source/MicroCity/MicroCity.ino
@@ -1,39 +1,40 @@
 #include <Arduboy2.h>
-//#include <EEPROM.h>
+#include <MicroGamerMemoryCard.h>
 #include "Draw.h"
 #include "Interface.h"
 #include "Game.h"
 #include "Simulation.h"
 
 Arduboy2Base arduboy;
+MicroGamerMemoryCard mem(sizeof(GameState) / 4 + 1);
 
 uint8_t GetInput()
 {
   uint8_t result = 0;
-  
+
   if(arduboy.pressed(A_BUTTON))
   {
-    result |= INPUT_A;  
+    result |= INPUT_A;
   }
   if(arduboy.pressed(B_BUTTON))
   {
-    result |= INPUT_B;  
+    result |= INPUT_B;
   }
   if(arduboy.pressed(UP_BUTTON))
   {
-    result |= INPUT_UP;  
+    result |= INPUT_UP;
   }
   if(arduboy.pressed(DOWN_BUTTON))
   {
-    result |= INPUT_DOWN;  
+    result |= INPUT_DOWN;
   }
   if(arduboy.pressed(LEFT_BUTTON))
   {
-    result |= INPUT_LEFT;  
+    result |= INPUT_LEFT;
   }
   if(arduboy.pressed(RIGHT_BUTTON))
   {
-    result |= INPUT_RIGHT;  
+    result |= INPUT_RIGHT;
   }
 
   return result;
@@ -63,38 +64,44 @@ void DrawBitmap(const uint8_t* bmp, uint8_t x, uint8_t y, uint8_t w, uint8_t h)
 
 void SaveCity()
 {
-//  uint16_t address = EEPROM_STORAGE_SPACE_START;
-//
-//  // Add a header so we know that the EEPROM contains a saved city
-//  EEPROM.update(address++, 'C'); 
-//  EEPROM.update(address++, 'T'); 
-//  EEPROM.update(address++, 'Y'); 
-//  EEPROM.update(address++, '1'); 
-//
-//  uint8_t* ptr = (uint8_t*) &State;
-//  for(size_t n = 0; n < sizeof(GameState); n++)
-//  {
-//    EEPROM.update(address++, *ptr);
-//    ptr++;
-//  }
+  uint8_t *data = (uint8_t *)mem.data();
+
+  // Add a header so we know that the EEPROM contains a saved city
+  *(data++) = (uint8_t)'C';
+  *(data++) = (uint8_t)'T';
+  *(data++) = (uint8_t)'Y';
+  *(data++) = (uint8_t)'1';
+
+  uint8_t* ptr = (uint8_t*) &State;
+  for(size_t n = 0; n < sizeof(GameState); n++)
+  {
+    *(data++) = *ptr;
+    ptr++;
+  }
+
+  //  Save RAM buffer to flash
+  mem.save();
 }
 
 bool LoadCity()
 {
-//  uint16_t address = EEPROM_STORAGE_SPACE_START;
-//
-//  if(EEPROM.read(address++) != 'C') return false;
-//  if(EEPROM.read(address++) != 'T') return false;
-//  if(EEPROM.read(address++) != 'Y') return false;
-//  if(EEPROM.read(address++) != '1') return false;
-//
-//  uint8_t* ptr = (uint8_t*) &State;
-//  for(size_t n = 0; n < sizeof(GameState); n++)
-//  {
-//    *ptr = EEPROM.read(address++);
-//    ptr++;
-//  }
-//
+  uint8_t *data = (uint8_t *)mem.data();
+
+  // Load data from flash to RAM
+  mem.load();
+
+  if(*(data++) != 'C') return false;
+  if(*(data++) != 'T') return false;
+  if(*(data++) != 'Y') return false;
+  if(*(data++) != '1') return false;
+
+  uint8_t* ptr = (uint8_t*) &State;
+  for(size_t n = 0; n < sizeof(GameState); n++)
+  {
+    *ptr = *(data++);
+    ptr++;
+  }
+
   return true;
 }
 
@@ -122,4 +129,3 @@ void loop()
     arduboy.display(false);
   }
 }
-
